5ae6eb1d4beb1092006ae2b70ed7b89fcbbd0bcd
B Monteiro <bluueem@gmail.com>
2023-06-22 03:24:54 +0100

    itemId

--- src/components/profile/state/newItemsState.ts
+++ src/components/profile/state/newItemsState.ts
@@ -1,11 +1,7 @@
 import { Item } from "./../../../server/utils/Item";
 import { atom } from "recoil";
 
-export interface newItem extends Item {
-  id: number;
-}
-
 export const newItemsState = atom({
   key: "collections",
-  default: [] as newItem[],
+  default: [] as Item[],
 });
